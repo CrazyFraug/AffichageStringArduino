a496cac25d4608721a3134cbc20fe78052d90e84
Arnaud Bozec <arnaud.bozec@m4x.org>
2015-11-13 15:11:52 +0100

    the library files

---
+++ StringUtil.cpp
@@ -0,0 +1,49 @@
+/*
+   String constructors
+ 
+ A. Bozec added d2Stringe and d2Stringf for float conversion
+ 
+ This example code is in the public domain.
+ */
+
+#include "StringUtil.h"
+
+String d2Stringe(double myDouble, int nbDecim1, int sizeMax)
+{
+   char acTmp[sizeMax+1];
+   unsigned char nbDecim2 = nbDecim1;
+   
+   // I limit the nb of decimals
+   // note: dtostre  limits decimals to 7
+   if (nbDecim1 > sizeMax-7) 
+       nbDecim2 = sizeMax-7;
+      
+   return String( dtostre(myDouble, acTmp, nbDecim2, DTOSTR_ALWAYS_SIGN) );
+}
+
+String d2Stringf(double myDouble, int nbDecim1, int sizeMax)
+ {
+   char acTmp[sizeMax+1];
+   unsigned char nbDecim2=nbDecim1;
+
+   if ( nbDecim1 > sizeMax-3 )  
+      // too many decimals
+      // decimal size: w + sign + 0 + . 
+      // ex: -0.0012\0
+      return d2Stringe(myDouble, nbDecim1, sizeMax);
+   else if ( fabs(myDouble) >  ldexp( 1, int(1+3.33*(sizeMax-nbDecim1-2)) ) ) 
+      // too big -> use exp notation
+      // decimal size: w + sign + . 
+      // ex: -12.3\0
+      // pow(10,sizeMax-nbDecim1-2) ~= pow(2, 3.33*(sizeMax-nbDecim1-2)) = ldexp(1, 3.33*(sizeMax-nbDecim1-2))
+      return d2Stringe(myDouble, nbDecim1, sizeMax);
+   else if ( fabs(myDouble) * ldexp( 1, int(1+3.33*nbDecim1) ) < 1 ) 
+      // too low,  decimals wont appear
+      return d2Stringe(myDouble, nbDecim1, sizeMax);
+   else 
+      nbDecim2 = nbDecim1;
+
+   return String( dtostrf(myDouble, -1, nbDecim2, acTmp) );
+ }
+
+ 
---
+++ StringUtil.h
@@ -0,0 +1,35 @@
+/*
+   String constructors
+ 
+ A. Bozec added d2Stringe and d2Stringf for float conversion
+ 
+ This example code is in the public domain.
+ */
+#ifndef STRINGUTIL_h
+#define STRINGUTIL_h
+ 
+#include <Arduino.h>
+
+String d2Stringe(double myDouble, int nbDecim1, int sizeMax=20);
+
+String d2Stringf(double myDouble, int nbDecim1, int sizeMax=20);
+
+
+template <class T>
+String buildMsgArray(int id, char *tag, T *array, int size) {
+  String inStringResult("{\"id\":");
+  inStringResult = inStringResult + id + ",\"" + tag + "\":[" ;
+  
+  for (int i=0; i<size; i++)
+    {
+	if (i>0)
+      inStringResult += ",";
+	inStringResult += String(array[i]);
+	}
+  
+  inStringResult += "]}";
+  
+  return inStringResult;  
+}
+
+#endif
